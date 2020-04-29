29c783f66ed92b784a8b836b7a3b8194f03e6b9c
VaidasMasys <vaidas.masys@gmail.com>
2020-04-29 09:17:55 +0300

    installed redux-devtools-extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,16 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { weatherReducer } from './marsApi/reducer';
 import thunk from 'redux-thunk';
 import { WeatherDataState } from './marsApi/types';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export interface IRootState {
     marsWeather: WeatherDataState;
 }
+
 const store = createStore<IRootState, any, any, any>(
     combineReducers({
         marsWeather: weatherReducer,
     }),
-    applyMiddleware(thunk),
+    composeWithDevTools(applyMiddleware(thunk)),
 );
 export default store;
