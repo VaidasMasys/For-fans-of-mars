ded991fe7890f8bad840709e1ca3ee30f4a21682
VaidasMasys <vaidas.masys@gmail.com>
2020-05-12 15:45:58 +0300

    installed react date picker, update gallery component with it

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { fetchRoverPhotos } from '../../store/marsApi/asyncActions';
 import styles from './Gallery.module.scss';
 import ActiveGalleryWindow from './ActiveGalleryWindow';
@@ -12,10 +14,23 @@ const Gallery = () => {
         dispatch(fetchRoverPhotos());
     }, []);
 
+    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
     return (
         <div className={styles.container}>
             <div className={styles.backgroundImage}></div>
             <div className={styles.photos}>
+                <div className={styles.datePicker}>
+                    <label className={styles.datePickerLabel}>Pick a date</label>
+                    <DatePicker
+                        selected={selectedDate}
+                        onChange={(date) => {
+                            setSelectedDate(date);
+                        }}
+                        maxDate={new Date()}
+                        dateFormat="yyyy-MM-dd"
+                    />
+                </div>
                 <div className={styles.activeGalleryWindow}>
                     <ActiveGalleryWindow />
                 </div>
